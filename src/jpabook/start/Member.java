dac0ab1861e048d7fc186dbc53f5c3d0d5774abd
kjnam <kyujnam@gmail.com>
2016-08-02 21:34:16 +0900

    feat: 객체맵핑

--- src/jpabook/start/Member.java
+++ src/jpabook/start/Member.java
@@ -1,12 +1,26 @@
 package jpabook.start;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Kj Nam
  * @since 2016-08-02
  */
+@Entity
+@Table(name="MEMBER")
 public class Member {
+
+    @Id
+    @Column(name = "ID")
     private String id;
+
+    @Column(name = "NAME")
     private String username;
+
+    // 매핑정보가 없는 필드
     private Integer age;
 
     public String getId() {
