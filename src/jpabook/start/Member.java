42bb3153d27f9b7374601bf9359bdc0726f4d7d7
kjnam <kyujnam@gmail.com>
2016-08-02 21:20:38 +0900

    init

---
+++ src/jpabook/start/Member.java
@@ -0,0 +1,35 @@
+package jpabook.start;
+
+/**
+ * @author Kj Nam
+ * @since 2016-08-02
+ */
+public class Member {
+    private String id;
+    private String username;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
